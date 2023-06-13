51f0486a93d682d35f3af8a68d6683f5b342032d
Tropped <tropped@gmail.com>
2023-06-14 01:58:45 +0200

    Get Sentence List

--- src/server/controllers/sentences.controller.js
+++ src/server/controllers/sentences.controller.js
@@ -1,6 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 import * as sentenceService from "../services/sentences.service.js";
 
+const DEFAULT_PER_PAGE = 20;
+const VALID_SORT_VALUES = ["asc", "desc"];
+
 /**
  * Get a list of sentences
  *
@@ -8,6 +11,18 @@ import * as sentenceService from "../services/sentences.service.js";
  * @param {Response} res
  */
 export async function getSentenceList(req, res) {
-  const sentences = await sentenceService.getSentenceList();
-  res.status(StatusCodes.OK).json(sentences);
+  let page = parseInt(req.query.page);
+  if (isNaN(page) || page < 1) return res.status(StatusCodes.BAD_REQUEST).end();
+
+  let perPage = parseInt(req.query.per_page);
+  if (isNaN(perPage) || perPage < 0) perPage = DEFAULT_PER_PAGE;
+
+  let category = req.query.category;
+  if (category) category = category.toLowerCase();
+
+  let sort = req.query.sort;
+  if (sort) sort = VALID_SORT_VALUES.find((criteria) => criteria === sort.toLowerCase());
+
+  const sentences = await sentenceService.getSentenceList(page, perPage, category, sort);
+  return res.status(StatusCodes.OK).json(sentences);
 }
--- src/server/services/sentences.service.js
+++ src/server/services/sentences.service.js
@@ -1,12 +1,29 @@
 import db from "../database/db.js";
 
 /**
- * Returns a list of all sentences
+ * Returns a list of sentences
+ *
+ * @param {number | null} page Page to return. If null, returns all sentences
+ * @param {number} perPage Number of sentences to return per page
+ * @param {string | null} category Category filter. If null, returns all categories
+ * @param {string | null} sort "asc" or "desc"
  * @returns {Promise<FirebaseFirestore.DocumentData[]>}
  */
-export async function getSentenceList() {
-  const sentenceCollection = db.collection("sentences");
-  const snapshot = await sentenceCollection.get();
+export async function getSentenceList(page, perPage, category, sort) {
+  let sentenceRef = db.collection("sentences");
+
+  if (category) {
+    sentenceRef = sentenceRef.where("category", "==", category);
+  } else if (sort) {
+    sentenceRef = sentenceRef.orderBy("category", sort);
+  }
+
+  if (page) {
+    const offset = (page - 1) * perPage;
+    sentenceRef = sentenceRef.limit(perPage).offset(offset);
+  }
+
+  const snapshot = await sentenceRef.get();
 
   const sentences = snapshot.docs.map((doc) => doc.data());
   return sentences;
