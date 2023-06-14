ea6e4a9bfd9330606a21048802d278175fc8a867
Tropped <tropped@gmail.com>
2023-06-15 00:59:30 +0200

    Added translation on the frontend

--- src/views/pages/sentences/js/detail.js
+++ src/views/pages/sentences/js/detail.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-const BASE_URL = "http://localhost:3000/api/sentences";
+const API_URL = "http://localhost:3000/api/sentences";
 
 async function detail() {
   const url = document.location.pathname;
   const id = url.split("/").at(-1);
 
-  const response = await fetch(`${BASE_URL}/${id}`);
+  const response = await fetch(`${API_URL}/${id}`);
 
   if (!response.ok) {
     return console.log("Error", response);
@@ -17,4 +17,18 @@ async function detail() {
   document.getElementById("sentence-category").innerHTML = sentence.category;
 }
 
+async function translate() {
+  const url = document.location.pathname;
+  const id = url.split("/").at(-1);
+
+  const response = await fetch(`${API_URL}/${id}/translation`);
+  const result = await response.json();
+
+  document.getElementById("sentence-translated-text").innerHTML = result.text;
+  document.getElementById("translate").style.display = "inline-block";
+
+  document.getElementById("btn-translate").disabled = "true";
+}
+
 detail();
+document.getElementById("btn-translate").onclick = translate;
