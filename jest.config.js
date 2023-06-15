707540625a1e580a028ecf7cd51bfc766ca773fd
Tropped <tropped@gmail.com>
2023-06-15 17:16:11 +0200

    Test coverage and coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ const config = {
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "coverage",
+    "tests",
+    "config",
+    "src/app.js",
+    "src/views",
+  ],
 };
 
 export default config;
