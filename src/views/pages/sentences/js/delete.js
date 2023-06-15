345a94137d778e3268ca0eae55abc7132e373319
Tropped <tropped@gmail.com>
2023-06-15 13:39:09 +0200

    Frontend error handling

--- src/views/pages/sentences/js/delete.js
+++ src/views/pages/sentences/js/delete.js
@@ -2,18 +2,25 @@ const API_URL = "/api/sentences";
 
 let id;
 
+function errorHandler(errorBody) {
+  document.getElementById("sentence").style.display = "none";
+  document.getElementById("error-card").style.display = "block";
+
+  document.getElementById("status-code").innerText = errorBody.status;
+  document.getElementById("error-message").innerText = errorBody.message;
+}
+
 async function detail() {
   const url = document.location.pathname;
-  console.log(document.location.hostname);
   id = url.split("/").at(-2);
 
   const response = await fetch(`${API_URL}/${id}`);
+  const sentence = await response.json();
 
   if (!response.ok) {
-    return console.log("Error", response);
+    return errorHandler(sentence);
   }
 
-  const sentence = await response.json();
   document.getElementById("sentence-text").innerText = sentence.text;
   document.getElementById("sentence-category").innerText = sentence.category;
   document.getElementById("detail-link").setAttribute("href", `/sentences/${id}`);
